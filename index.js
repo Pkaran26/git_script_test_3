1d7ded5feababa3ac98dfdb824fd8201aac80d71
Prateek Kaushik <39780074+Pkaran26@users.noreply.github.com>
2022-02-06 03:43:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res)=>{
   res.sendFile(`${__dirname}/views/index.html`)
 })
 
-app.listen(3001, ()=>{
-  console.log('test 1 running on 3001');
+app.listen(3003, ()=>{
+  console.log('test 3 running on 3003');
 })
